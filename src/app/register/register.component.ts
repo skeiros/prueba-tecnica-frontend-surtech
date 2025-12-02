843b74e9e1d77c62ddfbe62e8eb7bdc4be36877f
skeiros <fxdrones@gmail.com>
2025-12-02 15:08:22 -0300

    Adding FormBuilder and simple version w data

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -7,14 +7,45 @@ import {
   TranslatePipe,
   TranslateModule,
 } from "@ngx-translate/core";
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import { CommonModule } from "@angular/common";
 @Component({
   selector: "app-register",
-  imports: [MaterialModule, NgxMaskDirective, TranslatePipe, TranslateModule],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    NgxMaskDirective,
+    TranslatePipe,
+    TranslateModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: "./register.component.html",
   styleUrl: "./register.component.css",
 })
 export class RegisterComponent {
-  constructor(private translate: TranslateService) {}
+  registerForm: FormGroup;
+  showPassword: boolean = false;
+  showConfirmPassword: boolean = false;
+
+  constructor(private translate: TranslateService, private fb: FormBuilder) {
+      this.registerForm = this.fb.group({
+        fullname: ["", Validators.required],
+        email: ["", Validators.required],
+        phone_prefix: ["", Validators.required],
+        phone_number: ["", Validators.required],
+        date_of_birth: ["", Validators.required],
+        password: ["", Validators.required],
+        confirmPassword: ["", Validators.required],
+        country: ["", Validators.required],
+      });
+  }
+  submitForm() {
+    if (this.registerForm.valid) {
+      this.showAlert();
+    } else {
+      this.registerForm.markAllAsTouched();
+    }
+  }
   showAlert() {
     Swal.fire("Success!", "Your operation was successful.", "success");
   }
