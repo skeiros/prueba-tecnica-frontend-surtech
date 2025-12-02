4ce8401eb68bee4a36d19dd12bbe88c956adfa21
skeiros <fxdrones@gmail.com>
2025-12-02 14:25:04 -0300

    Adding ngx-mask and ngx-translate

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,32 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideSweetAlert2 } from '@sweetalert2/ngx-sweetalert2';
-
+import { provideEnvironmentNgxMask } from 'ngx-mask';
 import { routes } from './app.routes';
+import {provideTranslateService, provideTranslateLoader} from "@ngx-translate/core";
+import {provideTranslateHttpLoader} from "@ngx-translate/http-loader";
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    provideEnvironmentNgxMask(),
     provideSweetAlert2({
       // Optional configuration, e.g.,
       fireOnInit: false,
       dismissOnDestroy: true,
     }),
+    provideHttpClient(),
+    provideTranslateService({
+       loader: provideTranslateHttpLoader({
+        prefix: 'assets/i18n/',
+        suffix: '.json'
+      }),
+      fallbackLang: 'es',
+      lang: 'es'
+    })
+
+
   ]
 };
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
-import { MaterialModule } from '../material/materia.module';
-import Swal from 'sweetalert2';
+import { Component } from "@angular/core";
+import { MaterialModule } from "../material/materia.module";
+import Swal from "sweetalert2";
+import { NgxMaskDirective } from "ngx-mask";
+import {
+  TranslateService,
+  TranslatePipe,
+  TranslateModule,
+} from "@ngx-translate/core";
 @Component({
-  selector: 'app-register',
-  imports: [MaterialModule],
-  templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  selector: "app-register",
+  imports: [MaterialModule, NgxMaskDirective, TranslatePipe, TranslateModule],
+  templateUrl: "./register.component.html",
+  styleUrl: "./register.component.css",
 })
 export class RegisterComponent {
-
+  constructor(private translate: TranslateService) {}
   showAlert() {
-          Swal.fire('Success!', 'Your operation was successful.', 'success');
-        }
+    Swal.fire("Success!", "Your operation was successful.", "success");
+  }
 }
